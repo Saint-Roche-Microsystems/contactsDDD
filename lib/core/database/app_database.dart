f4ff07c6fce2778133c995f4cc1cce2cbed30cdb
devdiagon <fredericktimo@gmail.com>
2026-01-09 21:39:14 -0500

    feat: initial config for Drift ORM
    Currently, add_database.g.dart is not provided in the gihub repository

---
+++ lib/core/database/app_database.dart
@@ -0,0 +1,24 @@
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import '../schemas/contactos.schema.dart';
+
+part 'app_database.g.dart';
+
+@DriftDatabase(tables: [ContactosSchema])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 2;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'contactos.db'));
+    return NativeDatabase(file);
+  });
+}
---
+++ lib/core/schemas/contactos.schema.dart
@@ -0,0 +1,10 @@
+import 'package:drift/drift.dart';
+
+class ContactosSchema extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get nombre => text().withLength(min: 1, max: 100)();
+  TextColumn get descripcion => text()();
+  TextColumn get foto => text()();
+  TextColumn get correo => text().withLength(min: 1, max: 100)();
+  BoolColumn get esFavorito => boolean().withDefault(const Constant(false))();
+}
