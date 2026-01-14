a9ea6c47911e81901a70ac3cb33960d35a268466
devdiagon <fredericktimo@gmail.com>
2026-01-14 09:17:11 -0500

    feat: app can open other apps

--- lib/presentation/views/details/contact_info_section.dart
+++ lib/presentation/views/details/contact_info_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../widgets/call_button.dart';
 import '../../widgets/info_card.dart';
@@ -8,6 +9,23 @@ import '../../../domain/entities/contacto.dart';
 class ContactInfoSection extends StatelessWidget {
   final Contacto contacto;
   const ContactInfoSection({super.key, required this.contacto});
+
+  Future<void> _llamar(String telefono) async {
+    final Uri telUri = Uri(scheme: 'tel', path: telefono);
+    if (await canLaunchUrl(telUri)) {
+      await launchUrl(telUri);
+    }
+  }
+
+  Future<void> _enviarEmail(String email) async {
+    final Uri emailUri = Uri(
+      scheme: 'mailto',
+      path: email,
+    );
+    if (await canLaunchUrl(emailUri)) {
+      await launchUrl(emailUri);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +42,7 @@ class ContactInfoSection extends StatelessWidget {
                 icon: Icons.phone,
                 label: 'Llamar',
                 color: SaintColors.success,
+                onPressed: () => _llamar(contacto.telefono),
               ),
               SizedBox(width: 12),
 
@@ -32,6 +51,7 @@ class ContactInfoSection extends StatelessWidget {
                 icon: Icons.email,
                 label: 'Mensaje',
                 color: SaintColors.primary,
+                onPressed: () => _enviarEmail(contacto.correo),
               ),
             ],
           ),
